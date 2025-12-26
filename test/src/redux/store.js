f9a8ddcd0aa56b0a2b4d2c0f3bf3907004c0c52a
saarthack <ssarthak2003@outlook.com>
2025-12-26 19:31:57 +0530

    finalest

--- test/src/redux/store.js
+++
@@ -1,9 +0,0 @@
-// store.js
-import { configureStore } from '@reduxjs/toolkit';
-import wallpaperReducer from './wallpaperSlice';
-
-export const store = configureStore({
-  reducer: {
-    wallpaper: wallpaperReducer,
-  },
-});
